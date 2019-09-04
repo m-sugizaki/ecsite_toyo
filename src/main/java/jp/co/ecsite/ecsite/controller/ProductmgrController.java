eac31a0bd4689ac0c0a0aa0fa12440dde2510228
Honomi Masuoka <honomi-masuoka@cubesystem.co.jp>
2019-09-04 13:54:33 +0900

    9/4 13:54 ProductmgrContorller テスト前最終変更　早坂

--- src/main/java/jp/co/ecsite/ecsite/controller/ProductmgrController.java
+++ src/main/java/jp/co/ecsite/ecsite/controller/ProductmgrController.java
@@ -1,6 +1,8 @@
 package jp.co.ecsite.ecsite.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,9 +22,9 @@ import jp.co.ecsite.ecsite.model.ProdmgrModel;
 import jp.co.ecsite.ecsite.model.ProductCartModel;
 import jp.co.ecsite.ecsite.model.UserModel;
 import jp.co.ecsite.ecsite.service.ProdmgrService;
+//テスト
 
 @Controller
-@SessionAttributes({"productsearchresultsentity","productentity"})
 public class ProductmgrController {
 
 	@Autowired
@@ -53,11 +54,12 @@ public class ProductmgrController {
 				return "search";
 			}
 
+	//商品検索画面表示
 	@RequestMapping(value="/search", method=RequestMethod.POST, params="kensaku")
 	public String search(@ModelAttribute ProdmgrModel prodmgrModel, Model model) {
 		ProductSearchEntity productsearchentity = new ProductSearchEntity();
 
-		/*最低価格と最高価格の空文字チェックを行う
+		/* 最低価格と最高価格の空文字チェックを行う
 		if(prodmgrModel.getMinprice().isEmpty()||prodmgrModel.getMaxprice().isEmpty()) {
 			productsearchentity.setMinprice(0);
 			productsearchentity.setMaxprice(2147483647);
@@ -70,6 +72,8 @@ public class ProductmgrController {
 		}
 		}*/
 
+
+
 		//最低価格の空文字チェックを行い、入力された文字か最低額をentityに格納
 		if(prodmgrModel.getMinprice().isEmpty()){
 			productsearchentity.setMinprice(0);
@@ -78,6 +82,7 @@ public class ProductmgrController {
 				productsearchentity.setMinprice(Integer.parseInt(prodmgrModel.getMinprice()));
 			}catch(NumberFormatException e) {
 				model.addAttribute("numberformat", "書式が違います。半角数字で入力してください。");
+				return "search";
 			}
 		}
 		//最高価格の空文字チェックを行い、入力された文字か最高額をentityに格納
@@ -88,6 +93,7 @@ public class ProductmgrController {
 				productsearchentity.setMaxprice(Integer.parseInt(prodmgrModel.getMaxprice()));
 			}catch(NumberFormatException e) {
 				model.addAttribute("numberformat", "書式が違います。半角数字で入力してください。");
+				return "search";
 			}
 
 		}
@@ -115,18 +121,27 @@ public class ProductmgrController {
 		}
 
 		List<ProductSearchResultsEntity> productsearchresultsentity = prodmgrService.productSearch(productsearchentity);
+
+		//入力された商品名または、メーカー名、金額がなかった時の処理
+		if(productsearchresultsentity.size() == 0) {
+			model.addAttribute("noproduct", "該当商品がありませんでした。");
+			return "search";
+		}else {
 		//entityで帰ってきた値をJSPに返す処理↓
 		model.addAttribute("productsearchresultsentity", productsearchresultsentity);
 
 		return "search";
 	}
+	}
 
+	//商品詳細画面表示
 	@RequestMapping(value="/search", method=RequestMethod.POST, params="detail")
 	public String detail(@ModelAttribute ProdmgrModel prodmgrModel, Model model, RedirectAttributes attributes) {
 		ProductEntity productentity =prodmgrService.productDetail(prodmgrModel.getProduct_id());
-
 		List<ReviewEntity> reviewentity = prodmgrService.productReview(prodmgrModel.getProduct_id());
 
+		//画像表示用 (base64型に変換してエンティティのフィールドに格納)
+		productentity.setImagebase(Base64.getEncoder().encodeToString(productentity.getImage()));
 
 		model.addAttribute("productentity", productentity);
 		model.addAttribute("reviewentity", reviewentity);
@@ -135,7 +150,15 @@ public class ProductmgrController {
 		//List<ReviewEntity> reviewentity = prodmgrService.productReview(prodmgrModel.getProduct_id());
 		//model.addAttribute("productentity", productentity);
 		//model.addAttribute("reviewentity", reviewentity);
+
+		List<String> colorlist =Arrays.asList(productentity.getColor().split(","));
+		model.addAttribute("colorlist", colorlist);
+		List<String> sizelist =Arrays.asList(productentity.getSize().split(","));
+		model.addAttribute("sizelist", sizelist);
+
 		return "detail";
 	}
 
 }
+
+
