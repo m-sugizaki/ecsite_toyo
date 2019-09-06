f439da2bc84f50c0514aec24b89e93a31cd22c66
sayaka <sayaka-kubota@cubesystem.co.jp>
2019-09-06 13:34:28 +0900

    shipping_address_noに変更

--- src/main/java/jp/co/ecsite/ecsite/service/AccountService.java
+++ src/main/java/jp/co/ecsite/ecsite/service/AccountService.java
@@ -6,7 +6,7 @@ public interface AccountService {
 	void deletePayOne(String payment_no);
 	void updatePayOne(PaymentMethodEntity paymentmethodentity);
 	void insertAddressOne(ShippingAddressEntity shippingaddressentity);
-	void deleteAddressOne(String shipping_no);
+	void deleteAddressOne(String shipping_address_no);
 	void updateAddressOne(ShippingAddressEntity shippingaddressentity);
 	void updateAccountOne(UserEntity userentity);
 */
