b6ffb87c871b7f29d2a1e35466799d9a919362d0
IshidaRyosuke <ryosuke-ishida@cubesystem.co.jp>
2019-09-06 11:45:43 +0900

    ポップアップ内での期限初期表示用　11時45分　石田

--- src/main/java/jp/co/ecsite/ecsite/entity/PaymentMethodEntity.java
+++ src/main/java/jp/co/ecsite/ecsite/entity/PaymentMethodEntity.java
@@ -10,6 +10,11 @@ public class PaymentMethodEntity {
 		private String card_number;
 		private Date expiration_date;
 		private String card_holder_name;
+
+		//更新ポップアップ内での初期表示用、11時45分石田
+		private String expiration_year;
+		private String expiration_month;
+
 		public String getUser_id() {
 			return user_id;
 		}
@@ -45,6 +50,18 @@ public class PaymentMethodEntity {
 		}
 		public void setCard_holder_name(String card_holder_name) {
 			this.card_holder_name = card_holder_name;
+		}
+		public String getExpiration_year() {
+			return expiration_year;
+		}
+		public void setExpiration_year(String expiration_year) {
+			this.expiration_year = expiration_year;
+		}
+		public String getExpiration_month() {
+			return expiration_month;
+		}
+		public void setExpiration_month(String expiration_month) {
+			this.expiration_month = expiration_month;
 		}
 
 
