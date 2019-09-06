8e56ccdcc42fd6e53047fb1f9b210bb250c18d23
Daisuke Hayasaka <daisuke-hayasaka@cubesystem.co.jp>
2019-09-06 09:36:55 +0900

    9.6 9:30　早坂

--- src/main/java/jp/co/ecsite/ecsite/repository/AccountRepository.java
+++ src/main/java/jp/co/ecsite/ecsite/repository/AccountRepository.java
@@ -1,7 +1,31 @@
 package jp.co.ecsite.ecsite.repository;
 
-import java.io.Serializable;
+import jp.co.ecsite.ecsite.entity.PaymentMethodEntity;
+import jp.co.ecsite.ecsite.entity.ShippingAddressEntity;
+import jp.co.ecsite.ecsite.entity.UserEntity;
 
-public class AccountRepository implements Serializable {
+public interface AccountRepository {
+
+	//サービスから受け取ったデータを使って支払い方法テーブルに挿入するメソッド
+	void insertPayOne(PaymentMethodEntity paymentmethodentity);
+
+	//サービスから受け取ったデータを使って支払い方法テーブルを更新するメソッド
+	void updatePayOne(PaymentMethodEntity paymentmethodentity);
+
+	//サービスから受け取ったデータを使って支払い方法テーブルから行を削除するメソッド
+	void deletetPayOne(PaymentMethodEntity paymentmethodentity);
+
+	//サービスから受け取ったデータを使ってお届け先テーブルに挿入するメソッド
+	void insertAddressOne(ShippingAddressEntity shippingaddressentity);
+
+	//サービスから受け取ったデータを使ってお届け先テーブルを変更するメソッド
+	void updateAddressOne(ShippingAddressEntity shippingaddressentity);
+
+	//サービスから受け取ったデータを使ってお届け先テーブルから行を削除するメソッド
+	void deleteAddressOne(ShippingAddressEntity shippingaddressentity);
+
+	//サービスから受け取ったデータを使ってユーザーテーブルを変更するメソッド
+	void updateAccountOne(UserEntity userentity);
+
 
 }
