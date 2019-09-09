04d355f9e4d97c38c76a24df87b65988afff3746
sayaka <sayaka-kubota@cubesystem.co.jp>
2019-09-09 14:38:46 +0900

    コメントはずしました。

--- src/main/java/jp/co/ecsite/ecsite/service/AccountService.java
+++ src/main/java/jp/co/ecsite/ecsite/service/AccountService.java
@@ -1,15 +1,17 @@
 package jp.co.ecsite.ecsite.service;
 
+import jp.co.ecsite.ecsite.entity.PaymentMethodEntity;
+import jp.co.ecsite.ecsite.entity.ShippingAddressEntity;
 import jp.co.ecsite.ecsite.entity.UserEntity;
 
 public interface AccountService {
 
-	//void insertPayOne(PaymentMethodEntity paymentmethodentity);
+	void insertPayOne(PaymentMethodEntity paymentmethodentity);
 	void deletePayOne(int payment_no);
-	//void updatePayOne(PaymentMethodEntity paymentmethodentity);
-	//void insertAddressOne(ShippingAddressEntity shippingaddressentity);
+	void updatePayOne(PaymentMethodEntity paymentmethodentity);
+	void insertAddressOne(ShippingAddressEntity shippingaddressentity);
 	void deleteAddressOne(int shipping_address_no);
-	//void updateAddressOne(ShippingAddressEntity shippingaddressentity);
+	void updateAddressOne(ShippingAddressEntity shippingaddressentity);
 	void updateAccountOne(UserEntity userentity);
 
 }
