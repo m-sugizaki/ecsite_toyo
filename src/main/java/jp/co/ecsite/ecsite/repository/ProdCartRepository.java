02d52b4253d837d08e0e52e5a5515aad8a0122d1
sayaka <sayaka-kubota@cubesystem.co.jp>
2019-09-12 11:07:43 +0900

    ProdCartRepository.java

--- src/main/java/jp/co/ecsite/ecsite/repository/ProdCartRepository.java
+++ src/main/java/jp/co/ecsite/ecsite/repository/ProdCartRepository.java
@@ -1,5 +1,19 @@
 package jp.co.ecsite.ecsite.repository;
 
+import jp.co.ecsite.ecsite.entity.ProductCartEntity;
+
 public interface ProdCartRepository {
+
+	void updateCart(ProductCartEntity productcartentity);
+
+	void deleteCart(int product_cart_id);
+
+	void insertHistoryOne(ProductCartEntity productcartentity);
+
+	void insertCart(ProductCartEntity productcartentity);
+
+	ProductCartEntity findResultOne(int order_no);
+
+	void changeResultOne(int order_no);
 
 }
