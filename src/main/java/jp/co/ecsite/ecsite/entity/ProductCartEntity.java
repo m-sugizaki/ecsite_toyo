ca9a2d55044ee1bdaf5144b0dc1a1c1430202559
Honomi Masuoka <honomi-masuoka@cubesystem.co.jp>
2019-09-12 11:27:21 +0900

    フィールドの追加　11:20 升岡

--- src/main/java/jp/co/ecsite/ecsite/entity/ProductCartEntity.java
+++ src/main/java/jp/co/ecsite/ecsite/entity/ProductCartEntity.java
@@ -1,26 +1,44 @@
 package jp.co.ecsite.ecsite.entity;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
 
+//11:20 フィールドの追加（升岡）
 public class ProductCartEntity implements Serializable {
-	//productテーブル
-	private int price;
-	private String product_name;
-
-	//product_cartテーブル
-	private String product_cart_id;
 	private String user_id;
+	private int product_cart_id;
 	private String product_id;
+	private String product_name;
+	private BigDecimal price;
 	private int quantity;
 	private String size;
 	private String color;
-	private Date cart_regist_dt;
-	public int getPrice() {
-		return price;
+	private LocalDateTime cart_regist_dt;
+	private String payment_method;
+	private int payment_no;
+	private int shipping_address_no;
+	private int order_no;
+	private Date delivery_plan_dt;
+	private Date delivery_completion_dt;
+	public String getUser_id() {
+		return user_id;
 	}
-	public void setPrice(int price) {
-		this.price = price;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getProduct_cart_id() {
+		return product_cart_id;
+	}
+	public void setProduct_cart_id(int product_cart_id) {
+		this.product_cart_id = product_cart_id;
+	}
+	public String getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
 	}
 	public String getProduct_name() {
 		return product_name;
@@ -28,23 +46,11 @@ public class ProductCartEntity implements Serializable {
 	public void setProduct_name(String product_name) {
 		this.product_name = product_name;
 	}
-	public String getProduct_cart_id() {
-		return product_cart_id;
+	public BigDecimal getPrice() {
+		return price;
 	}
-	public void setProduct_cart_id(String product_cart_id) {
-		this.product_cart_id = product_cart_id;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public String getProduct_id() {
-		return product_id;
-	}
-	public void setProduct_id(String product_id) {
-		this.product_id = product_id;
+	public void setPrice(BigDecimal price) {
+		this.price = price;
 	}
 	public int getQuantity() {
 		return quantity;
@@ -64,11 +70,47 @@ public class ProductCartEntity implements Serializable {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	public Date getCart_regist_dt() {
+	public LocalDateTime getCart_regist_dt() {
 		return cart_regist_dt;
 	}
-	public void setCart_regist_dt(Date cart_regist_dt) {
+	public void setCart_regist_dt(LocalDateTime cart_regist_dt) {
 		this.cart_regist_dt = cart_regist_dt;
+	}
+	public String getPayment_method() {
+		return payment_method;
+	}
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+	public int getPayment_no() {
+		return payment_no;
+	}
+	public void setPayment_no(int payment_no) {
+		this.payment_no = payment_no;
+	}
+	public int getShipping_address_no() {
+		return shipping_address_no;
+	}
+	public void setShipping_address_no(int shipping_address_no) {
+		this.shipping_address_no = shipping_address_no;
+	}
+	public int getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
+	}
+	public Date getDelivery_plan_dt() {
+		return delivery_plan_dt;
+	}
+	public void setDelivery_plan_dt(Date delivery_plan_dt) {
+		this.delivery_plan_dt = delivery_plan_dt;
+	}
+	public Date getDelivery_completion_dt() {
+		return delivery_completion_dt;
+	}
+	public void setDelivery_completion_dt(Date delivery_completion_dt) {
+		this.delivery_completion_dt = delivery_completion_dt;
 	}
 
 
--- src/main/java/jp/co/ecsite/ecsite/model/ProductCartModel.java
+++ src/main/java/jp/co/ecsite/ecsite/model/ProductCartModel.java
@@ -3,12 +3,37 @@ package jp.co.ecsite.ecsite.model;
 
 import java.io.Serializable;
 
-
+//11:20 フィールドの追加(升岡)
 public class ProductCartModel implements Serializable {
 
+	private String product_cart_id;
+	private String product_id;
+	private String price;
 	private String quantity;
 	private String size;
 	private String color;
+	private String payment_method;
+	private String payment_no;
+	private String shiiping_address_no;
+	private String order_no;
+	public String getProduct_cart_id() {
+		return product_cart_id;
+	}
+	public void setProduct_cart_id(String product_cart_id) {
+		this.product_cart_id = product_cart_id;
+	}
+	public String getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
 	public String getQuantity() {
 		return quantity;
 	}
@@ -27,5 +52,30 @@ public class ProductCartModel implements Serializable {
 	public void setColor(String color) {
 		this.color = color;
 	}
+	public String getPayment_method() {
+		return payment_method;
+	}
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+	public String getPayment_no() {
+		return payment_no;
+	}
+	public void setPayment_no(String payment_no) {
+		this.payment_no = payment_no;
+	}
+	public String getShiiping_address_no() {
+		return shiiping_address_no;
+	}
+	public void setShiiping_address_no(String shiiping_address_no) {
+		this.shiiping_address_no = shiiping_address_no;
+	}
+	public String getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(String order_no) {
+		this.order_no = order_no;
+	}
+
 
 }
