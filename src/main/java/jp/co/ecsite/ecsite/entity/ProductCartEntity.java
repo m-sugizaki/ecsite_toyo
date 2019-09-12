bff23396702ee406565716d092315cd8b4eca62f
Honomi Masuoka <honomi-masuoka@cubesystem.co.jp>
2019-09-12 15:22:34 +0900

    フィールドの追加,完全更新版　9/12 15:20

--- src/main/java/jp/co/ecsite/ecsite/entity/ProductCartEntity.java
+++ src/main/java/jp/co/ecsite/ecsite/entity/ProductCartEntity.java
@@ -7,22 +7,26 @@ import java.util.Date;
 
 //11:20 フィールドの追加（升岡）
 public class ProductCartEntity implements Serializable {
+	//商品カートで使用するフィールド
 	private String user_id;
 	private int product_cart_id;
 	private String product_id;
-	private String product_name;
-	private BigDecimal price;
 	private int quantity;
 	private String size;
 	private String color;
-	private LocalDateTime order_dt;
 	private LocalDateTime cart_regist_dt;
+	//購入履歴で使用するフィールド
+	private int order_no;
+	private String order_date;
+	private String order_status;
 	private String payment_method;
 	private int payment_no;
 	private int shipping_address_no;
-	private int order_no;
 	private Date delivery_plan_dt;
 	private Date delivery_completion_dt;
+	//追加項目
+	private String product_name;
+	private BigDecimal price;
 	public String getUser_id() {
 		return user_id;
 	}
@@ -40,18 +44,6 @@ public class ProductCartEntity implements Serializable {
 	}
 	public void setProduct_id(String product_id) {
 		this.product_id = product_id;
-	}
-	public String getProduct_name() {
-		return product_name;
-	}
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
 	}
 	public int getQuantity() {
 		return quantity;
@@ -71,17 +63,29 @@ public class ProductCartEntity implements Serializable {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	public LocalDateTime getOrder_dt() {
-		return order_dt;
-	}
-	public void setOrder_dt(LocalDateTime order_dt) {
-		this.order_dt = order_dt;
-	}
 	public LocalDateTime getCart_regist_dt() {
 		return cart_regist_dt;
 	}
 	public void setCart_regist_dt(LocalDateTime cart_regist_dt) {
 		this.cart_regist_dt = cart_regist_dt;
+	}
+	public int getOrder_no() {
+		return order_no;
+	}
+	public void setOrder_no(int order_no) {
+		this.order_no = order_no;
+	}
+	public String getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(String order_date) {
+		this.order_date = order_date;
+	}
+	public String getOrder_status() {
+		return order_status;
+	}
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
 	}
 	public String getPayment_method() {
 		return payment_method;
@@ -101,12 +105,6 @@ public class ProductCartEntity implements Serializable {
 	public void setShipping_address_no(int shipping_address_no) {
 		this.shipping_address_no = shipping_address_no;
 	}
-	public int getOrder_no() {
-		return order_no;
-	}
-	public void setOrder_no(int order_no) {
-		this.order_no = order_no;
-	}
 	public Date getDelivery_plan_dt() {
 		return delivery_plan_dt;
 	}
@@ -118,6 +116,18 @@ public class ProductCartEntity implements Serializable {
 	}
 	public void setDelivery_completion_dt(Date delivery_completion_dt) {
 		this.delivery_completion_dt = delivery_completion_dt;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
 	}
 
 
