ad993a28791d3bf24190e5ed6298af08df143223
Honomi Masuoka <honomi-masuoka@cubesystem.co.jp>
2019-09-12 11:01:44 +0900

    ProdCartService/Impl 作成　9/12 11:00 升岡

--- src/main/java/jp/co/ecsite/ecsite/service/ProdCartService.java
+++ src/main/java/jp/co/ecsite/ecsite/service/ProdCartService.java
@@ -1,5 +1,19 @@
 package jp.co.ecsite.ecsite.service;
 
+import jp.co.ecsite.ecsite.entity.ProductCartEntity;
+/*
 public interface ProdCartService {
 
-}
+	//商品カートを更新する
+	void updateCart(ProductCartEntity productcartentity);
+	//商品カートの中の商品を削除する
+	void deleteCart(int product_cart_id);
+	//注文するのボタンを押したとき購入履歴に追加する
+	void insertHistoryOne(ProductCartEntity productcartentity);
+	//同じ内容でカートに入れる
+	void insertCart(ProductCartEntity productcartentity);
+	//注文キャンセルボタンが押されたときに１つの商品情報を取ってくる
+	ProductCartEntity findResultOne(int order_no);
+	//注文キャンセルの取り消しボタンが押されたときに注文状況をキャンセルにする
+	void changeResultOne(int order_no);
+}*/
--- src/main/java/jp/co/ecsite/ecsite/service/ProdCartServiceImpl.java
+++ src/main/java/jp/co/ecsite/ecsite/service/ProdCartServiceImpl.java
@@ -1,5 +1,42 @@
 package jp.co.ecsite.ecsite.service;
 
-public class ProdCartServiceImpl implements ProdCartService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-}
+import jp.co.ecsite.ecsite.repository.ProdCartRepository;
+/*
+@Service
+public class ProdCartServiceImpl implements ProdCartService {
+@Autowired
+ProdCartRepository prodCartRepository;
+
+	//商品カートを更新する
+		@Override
+		void updateCart(ProductCartEntity productcartentity) {
+			prodCartRepository.updateCart(productcartentity);
+		}
+		//商品カートの中の商品を削除する
+		@Override
+		void deleteCart(int product_cart_id) {
+			prodCartRepository.deleteCart(product_cart_id);
+		}
+		//注文するのボタンを押したとき購入履歴に追加する
+		@Override
+		void insertHistoryOne(ProductCartEntity productcartentity) {
+			prodCartRepository.insertHistoryOne(productcartentity);
+		}
+		//同じ内容でカートに入れる
+		@Override
+		void insertCart(ProductCartEntity productcartentity) {
+			prodCartRepository.insertCart(productcartentity);
+		}
+		//注文キャンセルボタンが押されたときに１つの商品情報を取ってくる
+		@Override
+		ProductCartEntity findResultOne(int order_no) {
+			prodCartRepository.findResultOne(order_no);
+		}
+		//注文キャンセルの取り消しボタンが押されたときに注文状況をキャンセルにする
+		void changeResultOne(int order_no) {
+			prodCartRepository.changeResultOne(order_no);
+		}
+}*/
